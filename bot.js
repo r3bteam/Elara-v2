5bab947fd69a80a3b65f17c2a7fd9bf7486c27f1
r3bteam <39262475+r3bteam@users.noreply.github.com>
2018-10-07 19:48:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -106,4 +106,232 @@ bot.on('voiceStateUpdate', (oldM, newM) => {
     }
   })
 });
+  bot.on("guildBanAdd", (guild, member) => {
+
+  bot.setTimeout(() => {
+
+    guild.fetchAuditLogs({
+
+        limit: 1,
+
+        type: 22
+
+      })
+
+      .then(audit => {
+
+        let exec = audit.entries.map(a => a.executor.username);
+
+        try {
+
+          let log = guild.channels.find('name', 'log');
+
+          if (!log) return;
+
+          bot.fetchUser(member.id).then(myUser => {
+
+          let embed = new Discord.RichEmbed()
+
+        .setAuthor(exec)
+
+        .setThumbnail(myUser.avatarURL)
+
+        .addField('- Banned User:',`**${myUser.username}**`,true)
+
+        .addField('- Banned By:',`**${exec}**`,true)
+
+        .setFooter(myUser.username,myUser.avatarURL)
+
+            .setTimestamp();
+
+          log.send(embed).catch(e => {
+
+            console.log(e);
+
+          });
+
+          });
+
+        } catch (e) {
+
+          console.log(e);
+
+        }
+
+      });
+
+  }, 1000);
+
+});
+ bot.on("guildBanRemove", (guild, member) => {
+  bot.setTimeout(() => {
+    guild.fetchAuditLogs({
+        limit: 1,
+        type: 22
+      })
+      .then(audit => {
+        let exec = audit.entries.map(a => a.executor.username);
+        try {
+          let log = guild.channels.find('name', 'log');
+          if (!log) return;
+          client.fetchUser(member.id).then(myUser => {
+          let embed = new Discord.RichEmbed()
+        .setAuthor("UnBanned")
+		.setColor('#36393e') 
+		 .setThumbnail(myUser.avatarURL)
+        .addField('UnBanned User',`**${myUser.username}**`,true)
+        .addField('UnBanned By',`**${exec}**`,true)
+        .setFooter(myUser.username,myUser.avatarURL)
+            .setTimestamp();
+          log.send(embed).catch(e => {
+            console.log(e);
+          });
+          });
+        } catch (e) {
+          console.log(e);
+        }
+      });
+  }, 1000);
+});
+
+
+
+
+
+
+
+
+      bot.on("roleDelete", role => {
+  bot.setTimeout(() => {
+    role.guild.fetchAuditLogs({
+        limit: 1,
+        type: 30
+      })
+      .then(audit => {
+        let exec = audit.entries.map(a => a.executor.username)
+        try {
+
+          let log = role.guild.channels.find('name', 'log');
+          if (!log) return;
+          let embed = new Discord.RichEmbed()
+            .setColor('#36393e')          
+            .setTitle(' - Role Deleted ')
+            .addField('Role Name', role.name, true)
+            .addField('Role ID', role.id, true)
+            .addField('Role Color', role.hexColor, true)
+            .addField('Role Permission', role.permissions, true)
+            .addField('By', exec, true)
+            .setColor('#36393e') 
+            .setTimestamp()
+          log.send(embed).catch(e => {
+            console.log(e);
+          });
+        } catch (e) {
+          console.log(e);
+        }
+      })
+  }, 1000)
+})
+
+
+bot.on('roleCreate', role => {
+  bot.setTimeout(() => {
+    role.guild.fetchAuditLogs({
+        limit: 1,
+        type: 30
+      })
+      .then(audit => {
+        let exec = audit.entries.map(a => a.executor.username)
+        try {
+
+          let log = role.guild.channels.find('name', 'log');
+          if (!log) return;
+          let embed = new Discord.RichEmbed()
+            .setTitle('+ Role Created')
+            .addField('Role Name', role.name, true)
+            .addField('Role ID', role.id, true)
+            .addField('Role Color', role.hexColor, true)
+            .addField('Role Permission', role.permissions, true)
+            .addField('By', exec, true)
+            .setColor('#36393e') 
+            .setTimestamp()
+          log.send(embed).catch(e => {
+            console.log(e);
+          });
+        } catch (e) {
+          console.log(e);
+        }
+      })
+  }, 1000)
+})
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+bot.on('messageDelete', message => {
+    if (!message || !message.id || !message.content || !message.guild || message.author.bot) return;
+    const channel = message.guild.channels.find('name', 'log');
+    if (!channel) return;
+   
+    let embed = new Discord.RichEmbed()
+       .setAuthor(`${message.author.tag}`, message.author.avatarURL)
+             .setTitle('Message Deleted')
+       .addField('Deleted Message',`${message.cleanContent}`)
+       .addField('Deleted In',`<#${message.channel.id}>`)
+       .addField('By', `<@${message.author.id}> `)
+       .setColor('#36393e')
+       .setTimestamp();
+     channel.send({embed:embed});
+ 
+});
+
+
+
+
+  bot.on('messageUpdate', (message, newMessage) => {
+    if (message.content === newMessage.content) return;
+    if (!message || !message.id || !message.content || !message.guild || message.author.bot) return;
+    const channel = message.guild.channels.find('name', 'log');
+    if (!channel) return;
+ 
+    let embed = new Discord.RichEmbed()
+       .setAuthor(`${message.author.tag}`, message.author.avatarURL)
+       .setTitle('Message Edited')
+       .addField('Before Edit',`${message.cleanContent}`)
+       .addField('After Edit',`${newMessage.cleanContent}`)
+       .addField('Edit In',`<#${message.channel.id}>`)
+       .addField('By', `<@${message.author.id}> `)
+       .setColor('#36393e')
+       .setTimestamp();
+     channel.send({embed:embed});
+ 
+ 
+});
+
+
+   bot.on("deleteChannel",  dc => {
+  const channel = dc.guild.channels.find("name", "log")
+  if(channel) {
+  var embed = new Discord.RichEmbed()
+  .setTitle(dc.guild.name)
+  .setDescription(`***Channel Deleted Name : *** **${dc.name}** ⬅️`)
+  .setColor(`RANDOM`)
+  .setTimestamp(); 
+  channel.sendEmbed(embed)
+  }
+  });
+
+
 bot.login(config.token);
